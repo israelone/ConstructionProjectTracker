3812ccbf71d6e66e158dce9fe186fad08e51c811
israelone <itzisraelone@gmail.com>
2026-03-30 19:06:47 -0500

    Made a small update to the config file

--- site.config.ts
+++ site.config.ts
@@ -1,3 +1,4 @@
 // Update this value if the GitHub repository name changes.
 // Example: '/my-new-repo-name/'
-export const githubPagesBasePath = '/ConstructionProjectTracker/'
+export const githubPagesBasePath =
+  process.env.NODE_ENV === "production" ? "/ConstructionProjectTracker/" : "/";
